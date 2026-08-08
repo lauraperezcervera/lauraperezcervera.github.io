a9944629cd7d0388e56a989cb3484323f6119636
lauraperezcervera <109283933+lauraperezcervera@users.noreply.github.com>
2026-08-08 19:37:48 +0000

    Deploying to gh-pages from @ lauraperezcervera/lauraperezcervera.github.io@7c7afd3274b327c8973e7e802b83e8a4837c1120 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -443,13 +443,6 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/assets/pdf/LPerezCervera_CV.pdf", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
